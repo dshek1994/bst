31c635308bf7542a58f031c2df97e194914445ef
Daniel S <dshekari@icloud.com>
2023-05-16 22:25:59 -0500

    Adding my BST project to github

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
+from BinarySearchTree import *
+
+def main():
+    nums = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
+    bst = BSTNode()
+    for num in nums:
+        bst.insert(num)
+    print("preorder:")
+    print(bst.preorder([]))
+    print("#")
+
+    print("postorder:")
+    print(bst.postorder([]))
+    print("#")
+
+    print("inorder:")
+    print(bst.inorder([]))
+    print("#")
+
+    bst.insert(22)
+
+    print(bst.preorder)
+
+
+main()
